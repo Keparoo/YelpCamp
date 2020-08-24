ea9db17c000337f1655694a562c029e1faeceaff
Kep <KepCreations@gmail.com>
2020-08-24 13:52:09 -0400

    fix listen port

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
+let port = process.env.PORT || 3000;
+app.listen(port, function(){
     console.log("The YelpCamp Server has started!");
 });
